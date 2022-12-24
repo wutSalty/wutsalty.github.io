28aeaad1003191be004125ccab49463b8522d08c
wutSalty <96165794+wutSalty@users.noreply.github.com>
2022-12-25 01:27:20 +1100

    Use Bootstrap 5.1.3 + Dark Mode mod
    Added some other stuff too

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-function loadContent() {
-    var contentDiv = this.document.getElementById("app");
-    var contentID = location.hash.substring(1);
-    contentDiv.innerHTML = contentID;
+//function loadContent() {
+    //var contentDiv = this.document.getElementById("app");
+    //var contentID = location.hash.substring(1);
+    //contentDiv.innerHTML = contentID;
+//}
+
+//if (!location.hash){
+    //location.hash = "#home";
+//}
+
+//loadContent();
+
+//window.addEventListener("hashchange", loadContent)
+
+var theme = "empty";
+var themeElement = document.getElementById("theme-sheet");
+var themeButton = document.getElementById("theme-toggle-btn");
+
+var lightSheet = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
+var darkSheet = "https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-night.min.css";
+
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches){
+    var theme = "dark";
+} 
+else {
+    var theme = "light";
 }
 
-if (!location.hash){
-    location.hash = "#home";
+if (localStorage.getItem("theme") === null) {
+    localStorage.setItem("theme", theme);
+}
+else {
+    theme = localStorage.getItem("theme");
 }
 
-loadContent();
+function setTheme() {
+    if (localStorage.getItem("theme") == "light"){
+        themeElement.href = lightSheet;
+        themeButton.innerHTML = "🌙 Dark";
+    } else {
+        themeElement.href = darkSheet;
+        themeButton.innerHTML = "☀ Light";
+    }
+}
 
-window.addEventListener("hashchange", loadContent)
+setTheme();
+
+function toggleTheme() {
+    if (theme == "light") {
+        theme = "dark";
+    }
+    else {
+        theme = "light";
+    }
+    localStorage.setItem("theme", theme);
+    setTheme();
+}
