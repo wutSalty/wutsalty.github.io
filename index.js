b65cd0783d2af60bdd34ac09a8dc9c2490c27522
wutSalty <96165794+wutSalty@users.noreply.github.com>
2023-01-14 20:16:58 +1100

    Added carousel of images

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
-//function loadContent() {
-    //var contentDiv = this.document.getElementById("app");
-    //var contentID = location.hash.substring(1);
-    //contentDiv.innerHTML = contentID;
-//}
-
-//if (!location.hash){
-    //location.hash = "#home";
-//}
-
-//loadContent();
-
-//window.addEventListener("hashchange", loadContent)
-
 //Theme manager
-var theme = "empty";
-var themeElement = document.getElementById("the-body");
-var themeButton = document.getElementById("theme-toggle-btn");
-var themeColor = document.getElementById("safari-theme-color");
+let theme = "empty";
+const themeButton = document.getElementById("theme-toggle-btn");
 
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches){
-    var theme = "dark";
-} 
-else {
-    var theme = "light";
+const firstLoad = () => {
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches){
+        theme = "dark";
+    } 
+    else {
+        theme = "light";
+    }
+    
+    if (localStorage.getItem("theme") === null) {
+        localStorage.setItem("theme", theme);
+    }
+    else {
+        theme = localStorage.getItem("theme");
+    }
 }
 
-if (localStorage.getItem("theme") === null) {
-    localStorage.setItem("theme", theme);
-}
-else {
-    theme = localStorage.getItem("theme");
-}
-
-function setTheme() {
+const setTheme = () => {
     if (localStorage.getItem("theme") == "light"){
         themeElement.setAttribute("data-bs-theme", "light");
         themeButton.innerHTML = "🌙 Dark";
@@ -41,16 +27,16 @@ function setTheme() {
         themeElement.setAttribute("data-bs-theme", "dark");
         themeButton.innerHTML = "☀ Light";
         themeColor.content = "#212529";
-    }
+    };
 }
 
+firstLoad();
 setTheme();
 
-function toggleTheme() {
+const toggleTheme = () => {
     if (theme == "light") {
         theme = "dark";
-    }
-    else {
+    } else {
         theme = "light";
     }
     localStorage.setItem("theme", theme);
@@ -59,16 +45,14 @@ function toggleTheme() {
 
 //Time ticker
 const timeText = document.getElementById("current-time");
-
-function replaceTime() {
+const replaceTime = (element) => {
     options = {
         weekday: "short", day: "numeric", month: "short", hour: "numeric", minute: "numeric", second: "numeric", timeZone: "Australia/Sydney", timeZoneName: 'short'
     }
     const finalDate = new Intl.DateTimeFormat('default', options).format(new Date());
-    timeText.innerHTML = finalDate;
+    element.innerHTML = finalDate;
 }
-
-setInterval(replaceTime, 1000);
+setInterval(replaceTime, 1000, timeText);
 
 //Tooltip intialise
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
